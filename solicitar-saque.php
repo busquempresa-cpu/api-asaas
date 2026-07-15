6d3328ed9683919bd4984cee3ec526f7c7c3e9bf
busquempresa-cpu <busquempresa@gmail.com>
2026-07-15 19:53:58 -0300

    Update solicitar-saque.php

--- solicitar-saque.php
+++ solicitar-saque.php
@@ -7,7 +7,7 @@ header("Content-Type: application/json");
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { exit(0); }
 
 $token_asaas = getenv('ASAAS_API_KEY');
-$asaasUrl = "https://sandbox.asaas.com/api/v3/payments";
+$asaasUrl = "https://sandbox.asaas.com/api/v3/transfers";
 
 $input = json_decode(file_get_contents("php://input"), true);
 
